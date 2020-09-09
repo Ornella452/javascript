2c0798e74f312c6fd8a94f395fbf5d706a779408
ourfi <Ornella93@outlook.fr>
2020-09-09 17:42:36 +0200

    Mis a jours exo blaxkjack

--- jours8/blackjack/blackjack.js
+++ jours8/blackjack/blackjack.js
@@ -1,30 +1,116 @@
 var sJoueur = 0;
 
-var sBanque = Math.floor(Math.random() * Math.floor(21 - 16) + 16)
-var card = Math.floor(Math.random() * Math.floor(11 - 1) + 1)
-console.log(card)
-console.log(sJoueur)
+var sBanque = Math.floor(Math.random() * (21 - 16) + 16)
+
+
 var prompt = require('prompt');
 
 prompt.start();
 
 function displayPrompt() {
-  prompt.get({ name: 'q', description: 'Card ?' }, function (err,result) {
+  prompt.get({ name: 'q', description: 'Card ?' }, function (err, result) {
 
     if (result.q === 'y' || result.q === 'yes') {
-       
-            sJoueur += card
-            console.log("Score du joueur : " + sJoueur)
-    
-        
-        }else if(result.q == 'passed' || result.q == 'p'){
+      var card = Math.floor(Math.random() * (11 - 1) + 1)
+      sJoueur += card
+      console.log("Score du joueur : " + sJoueur)
+      console.log("Tu as tiret la carte " + card)
 
-          console.log('Game overs : Fin de la partie ')
-          return;
+    } else if ((result.q == 'passed' || result.q == 'p') && (sJoueur > 16)) {
+
+      if (sJoueur > sBanque) {
+
+        console.log('Bravo tu as gagné')
+        console.log("Your score is " + sJoueur + ", bank is " + sBanque)
+        return
+
+      }
+
+      if (sJoueur < sBanque) {
+
+        console.log("Perdu ")
+        console.log("Your score is " + sJoueur + ", bank is " + sBanque)
+
+        return
+      }
+
+
     }
-    displayPrompt() 
+    if ((sJoueur > 21) || (sJoueur === sBanque)) {
+
+      console.log('Tu as perdu')
+
+      console.log("Your score is " + sJoueur + ", bank is " + sBanque)
+      return
+    }
+
+    if (sJoueur === 21) {
+
+      console.log('Bravo tu as fait un BlackJack !')
+      console.log("Your score is " + sJoueur + ", bank is " + sBanque)
+      return
+    }
+
+    displayPrompt()
 
   });
 }
 
 displayPrompt();
+
+// Jeux du black 
+
+
+/*
+var scorejoueur = 0;
+var scorebanque = Math.floor(Math.random() * (21 - 16) + 16)
+//var card = Math.floor(Math.random() * (11 - 1) + 1)
+//console.log(card)
+var prompt = require('prompt');
+prompt.start();
+function displayPrompt() {
+  prompt.get({ name: 'q', description: 'Card?' }, function (err, result) {
+    if ((result.q == 'Y' || result.q == 'YES') || (result.q == 'y' || result.q == 'yes')) {
+
+      var card = Math.floor(Math.random() * (11 - 1) + 1)
+      scorejoueur = scorejoueur + card
+      console.log("scorejoueur " + scorejoueur);
+      console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+
+    }
+    else if (result.q === 'p' || result.q === 'pass') {
+      //console.log("gagne")
+      console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+
+      if ((scorejoueur > scorebanque) && (scorejoueur < 21)) {
+
+        console.log("le joueur gagne.")
+        console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+        return
+      }
+      if (scorejoueur < scorebanque) {
+        console.log("le joueur perd.")
+        console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+        return
+      }
+
+
+      //return;
+    }
+    if (scorejoueur === 21) {
+      console.log("Black Jack et gagne")
+      return
+    }
+    if (scorejoueur > 21) {
+      console.log("tu es perdu")
+      console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+      return
+    }  if (scorejoueur === scorebanque) {
+      console.log("le joueur perd.")
+      console.log("Your score is " + scorejoueur + ", bank is " + scorebanque)
+      return
+    }
+    displayPrompt();
+  });
+}
+displayPrompt();*/
